4547d40812a73abc1fd16d1a22b3e6382a48c3fc
YongJOoOo <we072200@naver.com>
2024-03-03 19:57:32 +0900

    722dydwns/GroupMatching_App_Server_Springboot#4 feat : 회원가입, 로그인 API 서버 구현에 따른 app 소스코드 변경

--- app/src/main/java/com/example/appgrouppurchasemaching/intro/LoginFragment.kt
+++ app/src/main/java/com/example/appgrouppurchasemaching/intro/LoginFragment.kt
@@ -4,24 +4,20 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 import com.example.appgrouppurchasemaching.ServerInfo
 import com.example.appgrouppurchasemaching.board.BoardMainActivity
 import com.example.appgrouppurchasemaching.databinding.FragmentLoginBinding
-import okhttp3.FormBody
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.util.concurrent.TimeUnit
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 import kotlin.concurrent.thread
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 class LoginFragment : Fragment() { //로그인 프래그먼트
 
@@ -81,78 +77,65 @@ class LoginFragment : Fragment() { //로그인 프래그먼트
 
             // -> FB 로그인 시도
             val act = activity as MainActivity
-            act.FBLoginControl = true
-            act.FBLoginController(act.FBControl, loginId, loginPw) //호출
-
+            act.userId = loginId
+            act.userPw = loginPw
 
             //-> 서버 통신 작업 수행
             thread {
                 val client = OkHttpClient()
 
-                val site = "http://${ServerInfo.SERVER_IP}:8080/App_GroupCharge_Server/login_user.jsp"
+                val site = "http://${ServerInfo.SERVER_IP}:8080/users/login"
 
-                //1) 사용자가 로그인 시도한 데이터값을 우선 서버에 전달하기 위해 세팅
-                val builder1 = FormBody.Builder()
-                builder1.add("user_id", loginId)
-                builder1.add("user_pw", loginPw)
-                val formBody = builder1.build()
+                val jsonObj = JSONObject();
+                jsonObj.put("userId", act.userId)
+                jsonObj.put("userPw", act.userPw)
+                val mediaType = "application/json; charset=utf-8".toMediaType()
+                val requestBody = jsonObj.toString().toRequestBody(mediaType);
 
-                //2) 요청
-                val request = Request.Builder().url(site).post(formBody).build()
+                //요청 Request 생성 - POST 형식으로 세팅한 데이터 보냄
+                val request = Request.Builder().url(site).post(requestBody).build()
 
                 //3) 요청 실행 후 결과는 response로 받음
                 val response = client.newCall(request).execute() //요청에 대한 응답은 response로 받음
 
-                // 통신 성공 여부에 따른 분기
-                if(response.isSuccessful == true) { //통신 성공 시,
-                    //응답 결과 추출
-                    val result_text = response.body?.string()!!.trim()
+                //응답 결과 추출
+                val responseObj = JSONObject(response.body!!.string())
 
-                    if (result_text == "FAIL") { //서버로부터 받은 응답 결과값이 FAIL이면
-                        activity?.runOnUiThread {
-                            val dialogBuilder = AlertDialog.Builder(requireContext())
-                            dialogBuilder.setTitle("로그인 실패")
-                            dialogBuilder.setMessage("아이디나 비밀번호가 잘못되었습니다.")
-                            dialogBuilder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
-                                binding.loginId.setText("") //id칸 공백처리
-                                binding.loginPw.setText("") //pw칸 공백처리
-                                binding.loginId.requestFocus() //id에 Re포커싱줌줌                            }
-                            }
-                            dialogBuilder.show()
-                        }
-                    }
-                    else{ //서버로부터 받은 응답 결과값 = 로그인 성공
-                        activity?.runOnUiThread {
-                            val dialogBuilder = AlertDialog.Builder(requireContext())
-                            dialogBuilder.setTitle("로그인 성공")
-                            dialogBuilder.setMessage("로그인 성공하였습니다.")
-                            dialogBuilder.setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
-                                //사용자 정보를 Preferences에 저장 -> 이후 Preference 접근은 액티비티로 접근하면된다.
-                                                                        //이름 = login_data, 모드 = 이 앱 안에서 데이터 공유 목적
-                                val pref = activity?.getSharedPreferences("login_data", Context.MODE_PRIVATE)
-                                val editor = pref?.edit() //편집 사용
-
-                                editor?.putInt("login_user_idx", Integer.parseInt(result_text)) //서버로부터 받은 값을 int형변환 후 put 처리
-                                editor?.commit() //실행
-
-                                // 키보드 내리기
-                                val act = activity as MainActivity
-                                act.hideKeyboard() //호출
-
-                                //화면 전환 처리
-                                val boardMainIntent = Intent(requireContext(),BoardMainActivity::class.java)
-                                startActivity(boardMainIntent)
-                                activity?.finish()
-                            }
-                            dialogBuilder.show()
-                        }
-                    }
-                }else{ //통신 실패 시 작업 처리
+                if (response.code == 200) {
                     activity?.runOnUiThread {
                         val dialogBuilder = AlertDialog.Builder(requireContext())
-                        dialogBuilder.setTitle("로그인 오류")
-                        dialogBuilder.setMessage("로그인 오류 발생")
-                        dialogBuilder.setPositiveButton("확인", null)
+                        dialogBuilder.setTitle("로그인 성공")
+                        dialogBuilder.setMessage(responseObj.getString("message"))
+                        dialogBuilder.setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
+                            //사용자 정보를 Preferences에 저장 -> 이후 Preference 접근은 액티비티로 접근하면된다.
+                            //이름 = login_data, 모드 = 이 앱 안에서 데이터 공유 목적
+                            val pref = activity?.getSharedPreferences("login_data", Context.MODE_PRIVATE)
+                            val editor = pref?.edit() //편집 사용
+
+                            editor?.putInt("login_user_idx", responseObj.getInt("userIdx")) //서버로부터 받은 값을 int형변환 후 put 처리
+                            editor?.commit() //실행
+
+                            // 키보드 내리기
+                            val act = activity as MainActivity
+                            act.hideKeyboard() //호출
+
+                            //화면 전환 처리
+                            val boardMainIntent = Intent(requireContext(),BoardMainActivity::class.java)
+                            startActivity(boardMainIntent)
+                            activity?.finish()
+                        }
+                        dialogBuilder.show()
+                    }
+                } else {
+                    activity?.runOnUiThread {
+                        val dialogBuilder = AlertDialog.Builder(requireContext())
+                        dialogBuilder.setTitle("로그인 실패")
+                        dialogBuilder.setMessage(responseObj.getString("message"))
+                        dialogBuilder.setPositiveButton("확인") { dialogInterface: DialogInterface, i: Int ->
+                            binding.loginId.setText("") //id칸 공백처리
+                            binding.loginPw.setText("") //pw칸 공백처리
+                            binding.loginId.requestFocus() //id에 Re포커싱줌줌                            }
+                        }
                         dialogBuilder.show()
                     }
                 }
--- app/src/main/java/com/example/appgrouppurchasemaching/intro/NickNameFragment.kt
+++ app/src/main/java/com/example/appgrouppurchasemaching/intro/NickNameFragment.kt
@@ -3,21 +3,18 @@ package com.example.appgrouppurchasemaching.intro
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.example.appgrouppurchasemaching.ServerInfo
 import com.example.appgrouppurchasemaching.databinding.FragmentNickNameBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import okhttp3.FormBody
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 import kotlin.concurrent.thread
 
 class NickNameFragment : Fragment() { //닉네임 화면
@@ -62,29 +59,24 @@ class NickNameFragment : Fragment() { //닉네임 화면
             val act = activity as MainActivity
             act.userNickName = nickNameNickName
 
-            //FB에도 가입 시도
-            act.FBControl = true // 입력완료 클릭 시 컨트롤 변수 처리
-            act.FBController(act.FBControl, act.userId, act.userPw, act.userNickName) //호출
-
             //-> 서버와 통신 작업
             thread {
                 val client = OkHttpClient() //클라이언트 객체 생성
 
-                val site = "http://${ServerInfo.SERVER_IP}:8080/App_GroupCharge_Server/join_user.jsp"
+                val site = "http://${ServerInfo.SERVER_IP}:8080/users"
 
                 //서버로 보낼 '데이터'세팅
-                val builder1 = FormBody.Builder()
-                //JSP 파일에서 받기로한 이름, 여기 액티비티에서 보낼 변수 순으로 담는다.
-                builder1.add("user_id", act.userId)
-                builder1.add("user_pw", act.userPw)
-                builder1.add("user_nick_name", act.userNickName)
-
-                val formBody = builder1.build() //서버로 보낼 데이터를 'FormBody' 형태로 build 생성
+                val jsonObj = JSONObject();
+                jsonObj.put("userId", act.userId)
+                jsonObj.put("userPw", act.userPw)
+                jsonObj.put("userNickName", act.userNickName)
+                val mediaType = "application/json; charset=utf-8".toMediaType()
+                val requestBody = jsonObj.toString().toRequestBody(mediaType);
 
                 //요청 Request 생성 - POST 형식으로 세팅한 데이터 보냄
-                val request = Request.Builder().url(site).post(formBody).build()
+                val request = Request.Builder().url(site).post(requestBody).build()
 
-                //응답 요청하기 - 클라이언트는 요청(Request)를 실행한 뒤 받을 응답은 reponse로 받음
+                //응답 요청하기 - 클라이언트는 요청(Request)를 실행한 뒤 받을 응답은 response 받음
                 val response = client.newCall(request).execute()
 
                 if(response.isSuccessful == true) { //통신 정상적 처리 시,
